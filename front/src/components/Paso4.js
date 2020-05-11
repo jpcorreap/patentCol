164ebfa049163542f9c2611c223f16645910e5ee
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-05-11 11:44:16 -0500

    Para hacer el demo

--- front/src/components/Paso4.js
+++ front/src/components/Paso4.js
@@ -3,10 +3,21 @@ import PasoTitle from "./PasoTitle.js";
 
 function Paso4() {
   return (
-    <div className="paso">
+    <div className="paso text-center">
       <PasoTitle actual={4} nombre="Llenar formulario de la SIC" />
       <br />
       <br />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Microsoft_Word_2013_logo.svg/1200px-Microsoft_Word_2013_logo.svg.png"
+        width="150px"></img>
+      <br />
+      <p>
+        Descargue el formulario de la SIC haciendo clic{" "}
+        <a href="https://legalwatch-ce193.firebaseapp.com/assets/fotos/descargar.docx">
+          aquí
+        </a>
+        .
+      </p>
     </div>
   );
 }
