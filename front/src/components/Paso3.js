ec067c37e84871065cbb6a5ee92416ebb2dd7a10
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-05-11 11:36:41 -0500

    Paso 3 cambiado

--- front/src/components/Paso3.js
+++ front/src/components/Paso3.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import PatentsView from "./patents/PatentsView.js";
+import PatentScope from "./patents/PatentScope.js";
 import PasoTitle from "./PasoTitle.js";
 //import styled from "styled-components";
 //import { useCheckboxState, Checkbox } from "reakit/Checkbox";
@@ -22,8 +25,6 @@ function useInput(defaultValue) {
 }*/
 
 function Paso3() {
-  const [patentsView, setPatentsView] = useState([]);
-  const [patentScope, setPatentScope] = useState([]);
   const [googleUtilityPatents, setGoogleUtilityPatents] = useState([]);
   const [hasError, setErrors] = useState(false);
 
@@ -31,34 +32,9 @@ function Paso3() {
   const inputProps = useInput();
   const checkbox = useCheckboxState({ state: [] });
 */
-  // Hace GET de la API
-  useEffect(() => {
-    async function fetchPatentsView() {
-      const type = "";
-      const res = await fetch(
-        //`https://www.patentsview.org/api/patents/query?q={"patent_type":[${type}]}&f=["patent_number","patent_abstract","patent_title","patent_type","patent_year"]&s=[{"patent_number":"desc"}]`
-        `https://www.patentsview.org/api/patents/query?q={"_gte":{"patent_date":"2007-01-04"}}&f=["patent_number","patent_abstract","patent_title","patent_type"]&s=[{"patent_number":"desc"}]`
-      );
-      res
-        .json()
-        .then((res) => {
-          setPatentsView(res.patents);
-        })
-        .catch((err) => setErrors(err));
-    }
-    fetchPatentsView();
-  }, []);
 
   // Hace GET de la base de datos
   useEffect(() => {
-    fetch("/getPatentscope")
-      .then((res) => res.json())
-      .then((patentScope) => {
-        if (patentScope) {
-          setPatentScope(patentScope);
-        }
-      });
-
     fetch("/getGoogleUtilityPatents")
       .then((res) => res.json())
       .then((googleUtilityPatents) => {
@@ -72,81 +48,65 @@ function Paso3() {
     <div className="paso">
       <PasoTitle actual={3} nombre="Estado de la técnica" />
       <br />
-      <h3 className="text-warning">Patentsview:</h3>
-      <div>
-        <div className="container">
-          <div className="row justify-content-center">
-            {patentsView.map((patent) => (
-              <div
-                className="card border-warning mb-3 col-md-12"
-                key={patent.patent_number}>
-                <div className="card-header">
-                  <h4>{patent.patent_title}</h4>
-                </div>
-                <div className="card-body">
-                  <p>
-                    <strong>Type:</strong> {patent.patent_type}
-                  </p>
-                  <p className="card-text text-justify">
-                    {patent.patent_abstract}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+      <br />
+      <div className="row text-center">
+        <div className="col text-center">
+          <Link to={"paso3/patensview"}>
+            <button className="btn btn-primary">PatentScope</button>
+          </Link>
+        </div>
+        <div className="col text-center">
+          <Link to={"paso3/patentscope"}>
+            <button className="btn btn-info">PatentsView</button>
+          </Link>
+        </div>
+        <div className="col text-center">
+          <button className="btn btn-info">Google Utility Patents</button>
+        </div>
+        <div className="col text-center">
+          <button className="btn btn-info">Google Issued Patents</button>
+        </div>
+        <div className="col text-center">
+          <button className="btn btn-info">NASA Patents</button>
         </div>
       </div>
       <br />
-      <h3 className="text-success">Patentscope:</h3>
-      <div>
-        <div className="container">
-          <div className="row justify-content-center">
-            {patentScope.map((patent) => (
-              <div
-                className="card border-success mb-3 col-md-12"
-                key={patent._id}>
-                <div className="card-header">
-                  <h4>{patent.title}</h4>
-                </div>
-                <div className="card-body">
-                  <p>
-                    <strong>Date:</strong> {patent.date}.{" "}
-                    <strong>Reference:</strong>{" "}
-                    <a
-                      href={patent.link}
-                      target="_blank"
-                      rel="noopener noreferrer">
-                      Patentscope
-                    </a>
-                  </p>
-                  <p className="card-text text-justify">{patent.description}</p>
-                  <p>{patent.author}</p>
-                </div>
+
+      <PatentsView />
+      <Switch>
+        <Route exact path="./patentsview"></Route>
+        <Route exact path="./patentscope">
+          <PatentScope />
+        </Route>
+        <Route exact path="./googleutility">
+          <h3 className="text-info">Google Utility Patents:</h3>
+          <div>
+            <div className="container">
+              <div className="row justify-content-center">
+                {googleUtilityPatents.map((patent) => (
+                  <div
+                    className="card border-info mb-3 col-md-12"
+                    key={patent._id}>
+                    <div className="card-header">
+                      <h4>{patent.title}</h4>
+                    </div>
+                    <div className="card-body">
+                      <p>
+                        <strong>Date:</strong> {patent.date}
+                      </p>
+                      <p className="card-text text-justify">
+                        {patent.abstract}
+                      </p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
-        </div>
-      </div>
-      <h3 className="text-info">Google Utility Patents:</h3>
-      <div>
-        <div className="container">
-          <div className="row justify-content-center">
-            {googleUtilityPatents.map((patent) => (
-              <div className="card border-info mb-3 col-md-12" key={patent._id}>
-                <div className="card-header">
-                  <h4>{patent.title}</h4>
-                </div>
-                <div className="card-body">
-                  <p>
-                    <strong>Date:</strong> {patent.date}
-                  </p>
-                  <p className="card-text text-justify">{patent.abstract}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+        </Route>
+      </Switch>
+
+      <br />
     </div>
   );
 }
--- front/src/components/Paso4.js
+++ front/src/components/Paso4.js
@@ -6,11 +6,6 @@ function Paso4() {
     <div className="paso">
       <PasoTitle actual={4} nombre="Llenar formulario de la SIC" />
       <br />
-      <p>
-        Aquí pienso usar{" "}
-        <a href="https://github.com/puppeteer/puppeteer">Puppeteer</a>, que
-        genera PDFs fáciles desde Node.
-      </p>
       <br />
     </div>
   );
