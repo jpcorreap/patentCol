1286878c842d228d0fbf31c60b1c64bec7fce99f
Juan Otalora <jp.otalora10@uniandes.edu.co>
2020-05-12 12:17:52 -0500

    Reorganizacion de carpetas de componentes
    Se recomienda mejorar y dividir las diferentes carpetas de componentes para facilitar la lectura de codigo

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+{/* 
+  Aqui tambien cambiaria un poco el formato de las carpetas creo que queda mas simple si en la parte de componentes cada pequeña carpeta 
+  coge solo uno de los componentes para facilitar la lectura de codigo
+*/} 
 function Home() {
   return (
     <div>
